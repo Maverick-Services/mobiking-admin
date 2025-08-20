3f26cd7833f31989b0cdb6349718f7c0977d8dd8
Maverick-Services <maverickit123@gmail.com>
2025-08-20 13:37:47 +0530

    orders

--- src/app/admin/orders/components/AmountCards.jsx
+++ src/app/admin/orders/components/AmountCards.jsx
@@ -2,43 +2,8 @@ import PCard from '@/components/custom/PCard'
 import React from 'react'
 import { formatINRCurrency } from './../../../../lib/services/formatters';
 
-function AmountCards({ orders, data }) {
-    // if (!orders) return null
+function AmountCards({ data }) {
     if (!data) return null
-
-    // console.log(orders)
-
-    // const excludedStatuses = ['Cancelled', 'Rejected', 'Returned', 'Hold']
-
-    // const realOrders = orders.filter(o =>
-    //     !o.abondonedOrder &&
-    //     !excludedStatuses.includes(o.status)
-    // )
-
-    // const getCountAndAmount = (filterFn) => {
-    //     const filtered = realOrders.filter(filterFn)
-    //     const count = filtered.length
-    //     const amount = filtered.reduce((total, order) => total + (order.orderAmount || 0), 0)
-    //     return { count, amount }
-    // }
-
-    // const { count: totalOrders, amount: totalOrderAmount } = getCountAndAmount(() => true)
-    // const { count: regularOrders, amount: regularOrdersAmount } = getCountAndAmount(o => o.type === 'Regular')
-    // const { count: posOrders, amount: posOrdersAmount } = getCountAndAmount(o => o.type === 'Pos')
-    // const { count: codOrders, amount: codOrdersAmount } = getCountAndAmount(o => o.method === 'COD')
-    // const { count: onlineOrders, amount: onlineOrdersAmount } = getCountAndAmount(o => o.method === 'Online')
-    // const { count: cashOrders, amount: cashOrdersAmount } = getCountAndAmount(o => o.method === 'Cash')
-    // const { count: upiOrders, amount: upiOrdersAmount } = getCountAndAmount(o => o.method === 'UPI')
-
-    // const cards = [
-    //     { title: 'Total Orders', count: totalOrders, amount: totalOrderAmount },
-    //     { title: 'Regular Orders', count: regularOrders, amount: regularOrdersAmount },
-    //     { title: 'POS Orders', count: posOrders, amount: posOrdersAmount },
-    //     { title: 'COD Orders', count: codOrders, amount: codOrdersAmount },
-    //     { title: 'Online Orders', count: onlineOrders, amount: onlineOrdersAmount },
-    //     { title: 'Cash Orders', count: cashOrders, amount: cashOrdersAmount },
-    //     { title: 'UPI Orders', count: upiOrders, amount: upiOrdersAmount },
-    // ]
 
     const cards = [
         {
